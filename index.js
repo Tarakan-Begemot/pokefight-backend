fc0e61f904b8db30c135f1e442ce431b38435617
laird <laird@mail.com>
2022-08-08 17:10:51 +0200

    init

---
+++ index.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+const app = express();
+require('dotenv').config();
+const PORT = process.env.PORT || 3000;
+const mongoDB = process.env.CONNECT_SERVICE;
+const Card = require('./models/card');
+
+app.use(cors());
+
+mongoose.connect(mongoDB).then(console.log('All good with DB'));
+
+const db = mongoose.connection;
+
+db.on('error', console.error.bind(console, 'MongoDB connection error: '));
+
+app.get('/', (req, res) => {
+  res.send('PokeBack runs good');
+});
+
+app.get('/types', (req, res) => {
+  Card.find().distinct('type', (err, data) => res.send(data));
+});
+
+app.get('/max-values/:base', (req, res) => {
+  Card.find().distinct(`base.${req.params.base}`, (err, data) => res.send(data));
+});
+
+app.get('/pokedex/:id', (req, res) =>
+  Card.find({ id: req.params.id }, (err, data) => res.send(data)),
+);
+
+app.listen(PORT, (req, res) => {
+  console.log(`PokeBack runs good at port: ${PORT}`);
+});
